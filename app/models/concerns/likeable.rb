be4ff120bf6ab3520cbd6baeff305341f9296832
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-13 16:34:04 -0500

    created likeable concern

---
+++ app/models/concerns/likeable.rb
@@ -0,0 +1,23 @@
+module Likeable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many through_table,
+             dependent: :destroy,
+             class_name: through_table_class,
+             inverse_of: name.downcase
+    has_many :likers, through: through_table, source: name.downcase, counter_cache: :likes_count
+  end
+
+  class_methods do
+    private
+
+    def through_table
+      "user_liked_#{name.downcase}s".to_sym
+    end
+
+    def through_table_class
+      "Liked#{name}"
+    end
+  end
+end
