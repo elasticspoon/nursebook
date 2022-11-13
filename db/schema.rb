cb227ee3d6c6006c9ee39ab8c56a839bd1d6c3bc
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-13 16:41:03 -0500

    added counter cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_032037) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_202337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "liked_posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_liked_posts_on_post_id"
+    t.index ["user_id"], name: "index_liked_posts_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -41,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_032037) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "liked_posts", "posts"
+  add_foreign_key "liked_posts", "users"
   add_foreign_key "posts", "users"
 end
