1163d6656f701a7f8e569b05527f20c2d3887b3a
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-12 22:21:44 -0500

    scaffolded and migrated posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_031026) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_032037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_031026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
