0f12835ffe97563d145e40e73066bd11d2242ed3
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-13 20:07:05 -0500

    removed excess scaffoled stuff

--- app/controllers/liked_comments_controller.rb
+++ app/controllers/liked_comments_controller.rb
@@ -1,22 +1,8 @@
 class LikedCommentsController < ApplicationController
-  before_action :set_liked_comment, only: %i[ show edit update destroy ]
-
-  # GET /liked_comments or /liked_comments.json
-  def index
-    @liked_comments = LikedComment.all
-  end
+  before_action :set_liked_comment, only: %i[show destroy]
 
   # GET /liked_comments/1 or /liked_comments/1.json
   def show
-  end
-
-  # GET /liked_comments/new
-  def new
-    @liked_comment = LikedComment.new
-  end
-
-  # GET /liked_comments/1/edit
-  def edit
   end
 
   # POST /liked_comments or /liked_comments.json
@@ -25,23 +11,10 @@ class LikedCommentsController < ApplicationController
 
     respond_to do |format|
       if @liked_comment.save
-        format.html { redirect_to liked_comment_url(@liked_comment), notice: "Liked comment was successfully created." }
+        format.html { redirect_to liked_comment_url(@liked_comment), notice: 'Liked comment was successfully created.' }
         format.json { render :show, status: :created, location: @liked_comment }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @liked_comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /liked_comments/1 or /liked_comments/1.json
-  def update
-    respond_to do |format|
-      if @liked_comment.update(liked_comment_params)
-        format.html { redirect_to liked_comment_url(@liked_comment), notice: "Liked comment was successfully updated." }
-        format.json { render :show, status: :ok, location: @liked_comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @liked_comment.errors, status: :unprocessable_entity }
       end
     end
@@ -52,19 +25,20 @@ class LikedCommentsController < ApplicationController
     @liked_comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to liked_comments_url, notice: "Liked comment was successfully destroyed." }
+      format.html { redirect_to liked_comments_url, notice: 'Liked comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_liked_comment
-      @liked_comment = LikedComment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def liked_comment_params
-      params.require(:liked_comment).permit(:comment_id, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_liked_comment
+    @liked_comment = LikedComment.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def liked_comment_params
+    params.require(:liked_comment).permit(:comment_id, :user_id)
+  end
 end
