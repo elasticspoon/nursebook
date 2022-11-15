c15adb820bcc8b92a006cf4505e24b2b8218438b
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-14 22:35:57 -0500

    added custom uniqueness validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,32 @@
 class Friendship < ApplicationRecord
   belongs_to :user_one, class_name: 'User', counter_cache: :friends_cache
   belongs_to :user_two, class_name: 'User', counter_cache: :friends_cache
+
+  validate :check_existing_friendships, :check_existing_requests, on: :create
+
+  private
+
+  def check_existing_friendships
+    friendship = Friendship.where(
+      user_one:,
+      user_two:
+    ).or(Friendship.where(
+           user_one: user_two,
+           user_two: user_one
+         )).take
+
+    errors.add(:base, "Friendship already exists: #{friendship.inspect}") if friendship
+  end
+
+  def check_existing_requests
+    request = Request.where(
+      sender: user_two,
+      receiver: user_one
+    ).or(Request.where(
+           sender: user_one,
+           receiver: user_two
+         )).take
+
+    errors.add(:base, "Request already exists: #{request.inspect}") if request
+  end
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/BlockLength
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
@@ -8,4 +9,45 @@ RSpec.describe Friendship, type: :model do
     it { should belong_to(:user_one).class_name('User').counter_cache(:friends_cache) }
     it { should belong_to(:user_two).class_name('User').counter_cache(:friends_cache) }
   end
+
+  describe 'Validations' do
+    context 'when friendship already exists' do
+      let(:friendship_copy) { build(:friendship, user_one: friendship.user_one, user_two: friendship.user_two) }
+      let(:friendship_copy_reversed) do
+        build(:friendship, user_one: friendship.user_two, user_two: friendship.user_one)
+      end
+
+      it 'should not allow a friendship to be created if one already exists' do
+        expect(friendship_copy).to_not be_valid
+        expect(friendship_copy_reversed).to_not be_valid
+      end
+
+      it 'should add the Friendship model to the errors if a friendship already exists' do
+        friendship_copy.valid?
+        friendship_copy_reversed.valid?
+        expect(friendship_copy.errors[:base]).to include("Friendship already exists: #{friendship.inspect}")
+        expect(friendship_copy_reversed.errors[:base]).to include("Friendship already exists: #{friendship.inspect}")
+      end
+    end
+
+    context 'when request already exists' do
+      let(:request) { create(:request) }
+      let(:request_copy) { build(:request, sender: request.sender, receiver: request.receiver) }
+      let(:request_copy_reversed) { build(:request, sender: request.receiver, receiver: request.sender) }
+
+      it 'should not allow a request to be created if one already exists' do
+        expect(request_copy).to_not be_valid
+        expect(request_copy_reversed).to_not be_valid
+      end
+
+      it 'should add the Request model to the errors if a request already exists' do
+        request_copy.valid?
+        request_copy_reversed.valid?
+        expect(request_copy.errors[:base]).to include("Request already exists: #{request.inspect}")
+        expect(request_copy_reversed.errors[:base]).to include("Request already exists: #{request.inspect}")
+      end
+    end
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
