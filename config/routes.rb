c910d81e62f13c7445a8f48d043c794d7443ca18
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-21 17:43:32 -0500

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :liked_posts
   resources :posts
   devise_for :users
+  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
