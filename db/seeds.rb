36000f6a12efce2876820203f96b2f0b952c29c4
Yuri Bocharov <quesadillaman@gmail.com>
2022-11-21 17:43:55 -0500

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Post.destroy_all
+
+# User.create!(email: 'test@example.com', password: 'password')
+
+20.times do
+  user = User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+  5.times do
+    user.posts.create!(content: Faker::Lorem.paragraph)
+  end
+end
+
+5.times do
+  User.all.each do |user|
+    Post.all.sample.comments.create!(content: Faker::Lorem.paragraph, creator: user)
+  end
+end
+
+10.times do
+  User.all.each do |user|
+    Comment.all.sample.comments.create!(content: Faker::Lorem.paragraph, creator: user)
+  end
+end
+
+User.all.each do |user|
+  Post.all.sample(5).each { |post| user.liked_posts << post }
+  Comment.all.sample(10).each { |comment| user.liked_comments << comment }
+end
