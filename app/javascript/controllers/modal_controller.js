c0448b9152ab4d15b5878896ae00b4abd106748b
Yuri Bocharov <quesadillaman@gmail.com>
2023-04-30 21:10:41 -0400

    modal update

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,25 +3,30 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="modal"
 export default class extends Controller {
   static targets = ["modal", "innerField", "outerField"];
+  static values = {
+    username: String,
+  };
 
   connect() {
-    this.modalTarget.addEventListener('close', (e) => {
+    this.modalTarget.addEventListener("close", () => {
       this.close();
-    })
+    });
   }
 
   show() {
     this.modalTarget.showModal();
   }
 
-  setOuterValue() {
-    this.outerFieldTarget.value = this.innerFieldTarget.value
+  setOuterValue(value) {
+    if (value === "") {
+      this.outerFieldTarget.textContent = `Whats on your mind, ${this.usernameValue}?`;
+    } else {
+      this.outerFieldTarget.textContent = value;
+    }
   }
 
   close() {
-    if (this.innerFieldTarget.value !== 'default') {
-      this.setOuterValue();
-    }
-      this.modalTarget.close();
+    this.setOuterValue(this.innerFieldTarget.value);
+    this.modalTarget.close();
   }
 }
